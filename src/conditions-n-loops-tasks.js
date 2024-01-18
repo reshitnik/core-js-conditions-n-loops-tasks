9f969fbd6db88bc0baeb77e0b59683b774c3ba42
reshitnik <reshitnik@gmail.com>
2024-01-18 13:18:34 +0300

    add task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -158,8 +158,58 @@ function convertToRomanNumerals(num) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  const oldStr = numberStr;
+  let str = '';
+  for (let i = 0; i < oldStr.length; i += 1) {
+    switch (oldStr[i]) {
+      case '0':
+        str += 'zero';
+        break;
+      case '1':
+        str += 'one';
+        break;
+      case '2':
+        str += 'two';
+        break;
+      case '3':
+        str += 'three';
+        break;
+      case '4':
+        str += 'four';
+        break;
+      case '5':
+        str += 'five';
+        break;
+      case '6':
+        str += 'six';
+        break;
+      case '7':
+        str += 'seven';
+        break;
+      case '8':
+        str += 'eight';
+        break;
+      case '9':
+        str += 'nine';
+        break;
+      case '-':
+        str += 'minus';
+        break;
+      case '.':
+        str += 'point';
+        break;
+      case ',':
+        str += 'point';
+        break;
+      default:
+        str += '';
+    }
+    if (i < oldStr.length - 1) {
+      str += ' ';
+    }
+  }
+  return str;
 }
 
 /**
