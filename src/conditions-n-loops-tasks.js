9a54d0d81a1354b2c46ff97164a021a19e8f1e52
reshitnik <reshitnik@gmail.com>
2024-01-20 15:44:51 +0300

    add task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -430,8 +430,18 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const newArr = arr;
+  for (let i = 0; i < newArr.length; i += 1) {
+    for (let j = 0; j < newArr.length; j += 1) {
+      if (arr[i] < newArr[j]) {
+        const k = newArr[j];
+        newArr[j] = newArr[i];
+        newArr[i] = k;
+      }
+    }
+  }
+  return arr;
 }
 
 /**
